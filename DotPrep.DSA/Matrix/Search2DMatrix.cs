9a353920de5bdf1fb05ecd6cd06631125cafc473
Rubel Hosen <rhosen@epharmasolutions.com>
2022-03-31 00:06:44 +0600

    add matrix

---
+++ DotPrep.DSA/Matrix/Search2DMatrix.cs
@@ -0,0 +1,30 @@
+﻿namespace DotPrep.DSA.Matrix
+{
+    // https://leetcode.com/problems/search-a-2d-matrix/
+
+    public class Search2DMatrix
+    {
+        // since all the rows of the matrix are sorted
+        // it's very tempting to use a binary search on each row to find the target
+        // but if we pay attention we will see that columns are also sorted
+        // and we can use this unique property to reduce the search complexity to O(n)
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            var row = 0;
+            // we will start from the last index of the first row
+            var col = matrix[row].Length - 1;
+            // and continue searching till the first index of each row
+            while (row < matrix.Length && col >= 0)
+            {
+                if (matrix[row][col] == target) return true;
+                // if the current value is smaller than the target value then it is not present on the current row
+                // so we need to check the next rows
+                else if (matrix[row][col] < target) row++;
+                // if the current value is greater than target value then it is not present on current column
+                // so we need to check previous columns
+                else col--;
+            }
+            return false;
+        }
+    }
+}
---
+++ DotPrep.DSATest/Matrix/Search2DMatrixTest.cs
@@ -0,0 +1,46 @@
+﻿using DotPrep.DSA.Matrix;
+using Xunit;
+
+namespace DotPrep.DSATest.Matrix
+{
+    public class Search2DMatrixTest
+    {
+
+        Search2DMatrix _search2DMatrix;
+
+        public Search2DMatrixTest()
+        {
+            _search2DMatrix = new Search2DMatrix();
+        }
+
+        [Fact]
+        void Test1()
+        {
+            var mat = new int[][]
+            {
+                new int[] {1,3,5,7},
+                new int[] {10,11,16,20},
+                new int[] {23,30,34,60},
+            };
+            var target = 3;
+            var expected = true;
+            var actual = _search2DMatrix.SearchMatrix(mat, target);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        void Test2()
+        {
+            var mat = new int[][]
+            {
+                new int[] {1,3,5,7},
+                new int[] {10,11,16,20},
+                new int[] {23,30,34,60},
+            };
+            var target = 13;
+            var expected = false;
+            var actual = _search2DMatrix.SearchMatrix(mat, target);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
