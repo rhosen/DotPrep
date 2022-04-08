3d724337114185b0de42f09fdf2369f73513eae9
Rubel Hosen <rhosen@epharmasolutions.com>
2022-04-08 20:07:35 +0600

    add kth largest element solution

---
+++ DotPrep.DSA/Heap/KthLargest.cs
@@ -0,0 +1,27 @@
+﻿namespace DotPrep.DSA.Heap
+{
+    // https://leetcode.com/problems/kth-largest-element-in-a-stream/
+    public class KthLargest
+    {
+
+        PriorityQueue<int, int> minHeap;
+        int count;
+
+        public KthLargest(int k, int[] nums)
+        {
+            minHeap = new PriorityQueue<int, int>();
+            count = k;
+            foreach (var num in nums)
+            {
+                Add(num);
+            }
+        }
+
+        public int Add(int val)
+        {
+            minHeap.Enqueue(val, val);
+            if (minHeap.Count > count) minHeap.Dequeue();
+            return minHeap.Peek();
+        }
+    }
+}
---
+++ DotPrep.DSATest/Heap/KthLargestTest.cs
@@ -0,0 +1,34 @@
+﻿using DotPrep.DSA.Heap;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DotPrep.DSATest.Heap
+{
+    public class KthLargestTest
+    {
+
+        KthLargest _kthLargest;
+
+        public KthLargestTest()
+        {
+            var k = 3;
+            var arr = new int[] { 4, 5, 8, 2 };
+            _kthLargest = new KthLargest(k, arr);
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var expected = new List<int> { 4, 5, 5, 8, 8 };
+            var actual = new List<int> { };
+            actual.Add(_kthLargest.Add(3));
+            actual.Add(_kthLargest.Add(5));
+            actual.Add(_kthLargest.Add(10));
+            actual.Add(_kthLargest.Add(9));
+            actual.Add(_kthLargest.Add(4));
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
